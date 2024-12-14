acb7689eab594154d1a2d7e938aea3769a83d23e
Sehammahrous <Seham.mahrous.17.12@gmail.com>
2024-12-14 12:32:35 +0200

    Add files via upload

---
+++ s.py
@@ -0,0 +1,17 @@
+class Filehandler:
+    def read_file(self,file_path):
+        pass
+    def write_file(self,file_path,data):
+        pass
+    
+class Dataprocess:
+    def process_data(self,data):
+        pass
+    
+class DataAnalyzer:
+    def analye_data(self,data):
+        pass
+    
+class ReportGenerator:
+    def generate_report(self,data):
+        pass
