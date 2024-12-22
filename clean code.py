e36636393e366725c38f86b5b7032376d4809618
Sehammahrous <Seham.mahrous.17.12@gmail.com>
2024-12-22 21:26:52 +0200

    Add files via upload

---
+++ clean code.py
@@ -0,0 +1,9 @@
+def calculate_result(a,b,c):
+    result=0
+    for i in range(a):
+        if i % 2 ==0:
+            result += i*b
+        else:
+            result += i*b
+    return result 
+            
